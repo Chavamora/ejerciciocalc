f26b5527eb8cd8e46b77a554816beba278eeec66
Chavamora <sbetomora@gmail.com>
2024-09-03 12:37:04 -0600

    First commit

---
+++ calc.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+void menu();
+int suma(int,int);
+int resta(int,int);
+int multi(int, int);
+int div(int, int);
+
+int main() {
+menu();
+return 0;
+}
+
+void menu() {
+    printf("menu");
+}
+
+int suma(int a, int b) {
+    return 1;
+}
+
+int resta(int a, int b) {
+    return 1;
+}
+
+int multi(int a, int b) {
+    return 1;
+}
+
+int div(int a, int b) {
+    return 1;
+}
