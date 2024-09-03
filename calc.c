0606308ca96f3ae1b0a84385237ed1a6ee950646
vangutmygs <122757090+vangutmygs@users.noreply.github.com>
2024-09-03 13:08:51 -0600

    Update calc.c
    corrección suma 2

--- calc.c
+++ calc.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 void menu();
-int suma(int,int, resultado);
+int suma(int,int);
 int resta(int,int);
 int multi(int, int);
 int div(int, int);
@@ -15,14 +15,11 @@ void menu() {
     printf("menu");
 }
 
-int suma(int a, int b, resultado) {
-    printf("Introduce el primer numero: \n");
-    scanf("%d", &a);
-    printf("Introduce el segundo numero: \n");
-    scanf("%d", &b);
+int suma(int a, int b) {
+    int resultado
     resultado = a + b;
     printf("El resultado es %d\n", resultado);
-    return 1;
+    return resultado;
 }
 
 int resta(int a, int b) {
