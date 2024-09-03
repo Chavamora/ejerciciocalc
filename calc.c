e2cd5ad874d08c4b176a4b965df963409989ae84
chava.mora <30995398+Chavamora@users.noreply.github.com>
2024-09-03 12:54:25 -0600

    implementacion del menu

--- calc.c
+++ calc.c
@@ -12,7 +12,36 @@ return 0;
 }
 
 void menu() {
-    printf("menu");
+    printf("Escoge la operacion 1=resta, 2=suma, 3=multiplicaion, 4=division, 5=modulo");
+    int opcion = 0;
+    scanf("%d", &opcion);
+    printf("ingresa el primer numero: ");
+    int a =0;
+    scanf("%d",&a);
+    printf("ingresa el segundo numero: ");
+    int b =0;
+    scanf("%d",&b);
+    switch(opcion) {
+        case 1:
+            suma(a,b);
+        break;
+        case 2:
+            resta(a,b);
+        break;
+        case 3:
+            multi(a,b);
+        break;
+        case 4:
+            div(a,b);
+        break;
+        case 5:
+            modulo(a,b);
+        break;
+        default:
+            printf("opcion invalida);
+        break;
+    }
+    
 }
 
 int suma(int a, int b) {
